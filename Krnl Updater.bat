53b726b43291570b5c967c0f0edde385156d6093
Los-3r <66935939+Los-3r@users.noreply.github.com>
2021-03-14 18:27:48 -0400

    Update Krnl Updater.bat

--- Krnl Updater.bat
+++ Krnl Updater.bat
@@ -8,18 +8,18 @@ REM | | \ \| |____ / ____ \| |__| | | |_) | |____| |___| |__| | \  /\  /
 REM |_|  \_\______/_/    \_\_____/  |____/|______|______\____/   \/  \/    
 
 REM Hello there, welcome to the KRNL updater.
-REM Set the below variable to 0, and then switch every instance of "Lucien" with your windows username to use this.
-set switch=0
+REM Set the below variable to 0, and then switch every instance of "USER" with your windows username to use this.
+set switch=1
 
 if %switch%==1 echo Right click this file then press edit & pause & exit
-cd C:/Users/Lucien/Desktop
+cd C:/Users/USER/Desktop
 mkdir KRNL
 
 echo Press Keep, Downloading Bootstrapper in 5 seconds
 ping 127.0.0.1 -n 6 > nul
 start "" https://keshhub.com/bootstrapper/krnl_console_bootstrapper.exe
 ping 127.0.0.1 -n 6 > nul
-move "C:\Users\Lucien\Downloads\krnl_console_bootstrapper.exe" "C:\Users\Lucien\Desktop\KRNL"
+move "C:\Users\USER\Downloads\krnl_console_bootstrapper.exe" "C:\Users\USER\Desktop\KRNL"
 ping 127.0.0.1 -n 3 > nul
 cd KRNL
 start krnl_console_bootstrapper.exe
